8f148dfa7fa8ccf9b0bbaf84456f77093535ef7d
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2025-08-01 14:10:31 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/senagokhan/legal_case_file_upload_search/LegalCaseFileUploadSearchApplication.java
@@ -0,0 +1,13 @@
+package com.senagokhan.legal_case_file_upload_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalCaseFileUploadSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalCaseFileUploadSearchApplication.class, args);
+	}
+
+}
